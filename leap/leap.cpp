c948a1ae00839d0552cfed6f0217fd187aba36f7
Manjunath K <manjunathmayya@gmail.com>
2021-05-07 14:37:04 +0530

    Completed Leap year Kata

---
+++ leap/leap.cpp
@@ -0,0 +1,20 @@
+#include "leap.h"
+
+namespace leap 
+{
+    bool is_leap_year(int year)
+    {
+        if(year % 400 == 0)
+            return true;
+
+        if(year % 100 == 0)
+            return false;
+
+        if(year % 4 == 0)
+            return true;
+        
+        else
+            return false;
+    }
+
+}  
